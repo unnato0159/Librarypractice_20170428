691f75f0e83a3ae45609c1bc35cc85309d79c48e
unnato0159 <unnato01@naver.com>
2019-04-28 14:30:49 +0900

    권한 설정 라이브러리 사용 예시  - Manifest 에 원하는 권한을 모두 적자  - 마쉬멜로이후의 대응으로 테드 퍼미션을 라이브러리에서 다시 받자

--- app/src/main/java/com/tje/librarypractice_20170428/MainActivity.java
+++ app/src/main/java/com/tje/librarypractice_20170428/MainActivity.java
@@ -1,11 +1,20 @@
 package com.tje.librarypractice_20170428;
 
+import android.Manifest;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
 import com.tje.librarypractice_20170428.databinding.ActivityMainBinding;
+
+import java.util.List;
 
 public class MainActivity extends BaseActivity {
 
@@ -23,6 +32,36 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+
+        act.callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PermissionListener permissionListener = new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted() {
+                        Uri uri = Uri.parse(String.format("Tel:%s",act.phoneNumTxt.getText().toString()));// 띄어 쓰기 하면 앱이 죽음  파싱 실패
+                        Intent intent = new Intent (Intent.ACTION_CALL,uri);
+                        startActivity(intent);
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(List<String> deniedPermissions) {
+                        Toast.makeText(mContext, "전화 권한이 거부되어 통화에 실패 했습니다. ", Toast.LENGTH_SHORT).show();
+                    }
+                };
+
+                TedPermission.with(mContext)
+                        .setPermissionListener(permissionListener)
+                        .setDeniedMessage("전화 권한을 거부 하면 통화가 불가합니다. [설정]에서 활성화 해주세요 ")
+                        .setPermissions(Manifest.permission.CALL_PHONE)
+                        .check();
+
+
+
+
+            }
+        });
 
     }
 
