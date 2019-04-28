9724d3336288aa06e603028896a9c2dc9b2c614f
unnato0159 <unnato01@naver.com>
2019-04-28 13:33:18 +0900

    글라이드 사용예시

--- app/src/main/java/com/tje/librarypractice_20170428/MainActivity.java
+++ app/src/main/java/com/tje/librarypractice_20170428/MainActivity.java
@@ -1,14 +1,20 @@
 package com.tje.librarypractice_20170428;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
+import com.tje.librarypractice_20170428.databinding.ActivityMainBinding;
+
 public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding act;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
         bindViews();
         setupEvents();
         setValues();
@@ -22,11 +28,14 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+        // 웹상의 피자헛 이미지를 이미지뷰에 띄우기.
+        Glide.with(mContext).load("https://mblogthumb-phinf.pstatic.net/20141124_182/howtomarry_1416806028308979cg_PNG/Pizza_Hut_logo.svg.png?type=w2").into(act.pizzaLogoImgView);
 
     }
 
     @Override
     public void bindViews() {
+        act = DataBindingUtil.setContentView(this,R.layout.activity_main);
 
     }
 }
